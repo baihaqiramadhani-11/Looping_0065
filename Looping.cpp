bc563a6c9639c61e092383a68b0da38b4ff682ca
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-31 15:38:17 +0700

    srand

--- Looping.cpp
+++ Looping.cpp
@@ -8,3 +8,7 @@ int main(){
     for(int i=0; i <= 4; i++){
         cout <<"Teknologi Informasi UMY" << endl;
     }
+    cout << endl;
+  srand(time(0));
+    cout << "PERULANGAN WHILE"<< endl;
+    x = 1 + rand()%10;
