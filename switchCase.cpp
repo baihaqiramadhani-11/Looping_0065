b12a63ccf436888e9cf6c07a5e413045ab76283f
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-31 15:47:20 +0700

    int main

--- switchCase.cpp
+++ switchCase.cpp
@@ -40,3 +40,9 @@ void output() {
     cout << "Status Kuota    : " << statusKuota << endl;
     cout << "----------------------------------" << endl;
 }
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+    return 0;
+}
